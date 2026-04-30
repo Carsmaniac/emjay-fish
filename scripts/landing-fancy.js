d690fc518438c93ee852dd85c3f2cede9ac82064
Carsmaniac <mickledapickle@me.com>
2026-04-30 15:09:48 +1000

    Add landing page
    The homepage has been replaced with a landing page that directs the visitor to all of my various websites.

---
+++ scripts/landing-fancy.js
@@ -0,0 +1,12 @@
+var body = document.getElementsByTagName("html")[0];
+var hue = 230;
+
+function changeBackground() {
+    hue += 1;
+    if (hue > 360) {
+        hue = 0;
+    }
+    body.style.background = "hsl(" + hue + ", 50%, 66%)";
+}
+
+setInterval(changeBackground, 200);
